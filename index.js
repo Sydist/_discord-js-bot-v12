96f0aeac27b51c0acd40b8f17cf85c0b85d5d57b
Sydist <sydistbot.personal@gmail.com>
2022-04-21 18:40:46 +0200

    Updates index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const { existsSync } = require("fs");
+
 require("dotenv").config();
 const { TOKEN, PREFIX } = process.env;
 
 const { Client } = require("discord.js");
 const client = new Client();
-
-const { existsSync } = require("fs");
 
 
 client.on("ready", () => console.log("Started..."));
@@ -13,7 +13,7 @@ client.on("message", message => {
    if (!message.content.startsWith(PREFIX)) return;
 
    const args = message.content.slice(1).split(/\s+/);
-   const name = args[0];
+   const name = args.shift();
 
    if (!existsSync(`./commands/${name}.js`)) return;
    require(`./commands/${name}`)(message, args);
